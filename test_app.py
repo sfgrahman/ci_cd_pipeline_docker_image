615a3271e292bde74a24c9cb1824c33df87caea6
sfgrahman <sfgrahman35@gmail.com>
2024-09-19 13:06:37 +0600

    fixed test file

--- test_app.py
+++ test_app.py
@@ -1,8 +1,7 @@
 from app import app
 
-
 def test_home():
-    response=app.test_client.get("/")
+    response=app.test_client().get("/")
 
-    assert response.status_code==2000
-    assert response.data == b"Hellow World"
+    assert response.status_code==200
+    assert response.data== b"Hello World!"
